9d4767278f31a7364fdb6f9d623a6122950d4c5b
simeon <s.kredatus@sportradar.com>
2020-06-02 11:00:53 +0200

    default push

---
+++ src/test/java/com/sportradar/code/retreat/SampleTest.java
@@ -0,0 +1,13 @@
+package com.sportradar.code.retreat;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+
+    @Test
+    public void GIVEN_nothing_WHEN_testing_THEN_failing() {
+
+        assertEquals(false, true);
+    }
+}
